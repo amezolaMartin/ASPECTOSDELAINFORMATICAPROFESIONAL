bd7d3b1f46a88a6e9014ab2670917a62efbf8a1b
Duxon900 <quintanojon@gmail.com>
2021-10-04 11:58:25 +0200

    proiektua hasieratu

--- app/src/main/java/ProiektoJosu/Main.java
+++ app/src/main/java/ProiektoJosu/Main.java
@@ -1,4 +1,11 @@
 package ProiektoJosu;
 
+
+/***
+ * Proiektu honetan fdisk ubuntu komandoa landuko da, honen interfaze grafikoa sortzeko
+ * komandoa oso intuitiboa ez denez, eskertzekoa izango litzateke GUI sinple bat izatea,
+ * guztiok diskak modifikatzearen beldur garelako eta ere grafiko batean segurtasun handiagoa dugu :)
+ */
 public class Main {
+
 }
